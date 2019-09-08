0f291988bb0c6951f3b79c74d4c14b9ecfddb582
Alexey Radul <axch@mit.edu>
2019-09-07 20:35:01 -0400

    Ran rake rails:update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Numbergossip::Application.config.secret_token = 'ca405d16ee75f7271fe8cd93fb69ede6d595de298463030a7a09c0ad26eece66a352d6409a41171c57b98e3e82ae825f04df1ff7b6bc055dda95d4536de4286f'
