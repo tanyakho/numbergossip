02f76039b9214cc175d592fbfd08aa5a66334244
Alexey Radul <axch@mit.edu>
2019-09-07 21:31:45 -0400

    Modern routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,75 @@
-ActionController::Routing::Routes.draw do |map|
+Numbergossip::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  # map.connect '', :controller => "welcome"
 
-  # Allow downloading Web Service WSDL as a file with an extension
-  # instead of a file named 'wsdl'
-#  map.connect ':controller/service.wsdl', :action => 'wsdl'
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-  map.connect ':action/:id', :controller => "number_gossip", :requirements => { :action => /index|list|credits|links|contact|editorial_policy|status|update/ }
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
+
+  match 'index(/:number)' => 'number_gossip#index'
+  match 'list' => 'number_gossip#list'
+  match 'credits' => 'number_gossip#credits'
+  match 'links' => 'number_gossip#links'
+  match 'contact' => 'number_gossip#contact'
+  match 'editorial_policy' => 'number_gossip#editorial_policy'
+  match 'status' => 'number_gossip#status'
+  match 'update(/:id)' => 'number_gossip#update'
+
+  # map.connect ':action/:id', :controller => "number_gossip", :requirements => { :action => /index|list|credits|links|contact|editorial_policy|status|update/ }
 
   # This arrangement permits the system to handle arbitrary paths as
   # attempts to query for numbers, which receive graceful errors.
-  map.number_page ':number', :controller => "number_gossip", :action => "index"
+  match ':number' => 'number_gossip#index', :as => :number_page
+  # map.number_page ':number', :controller => "number_gossip", :action => "index"
 
 end
