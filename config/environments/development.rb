69ea8960443285774c693240fdd4f97386f083b6
Alexey Radul <axch@mit.edu>
2019-09-22 09:50:55 -0400

    Whiny nils are gone.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,6 @@ Numbergossip::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,9 +7,6 @@ Numbergossip::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
-
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
