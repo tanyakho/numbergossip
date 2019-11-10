b42ed6bce5c377e99db1f4dd5d3e0de41b851851
Alexey Radul <axch@mit.edu>
2019-11-09 22:50:03 -0500

    Rails update task.

--- config/application.rb
+++ config/application.rb
@@ -70,5 +70,10 @@ module Numbergossip
 
     # Initialize configuration defaults
     config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
