88917890c02bc7880e3f5b93fbd5b14ee98cee20
Alexey Radul <axch@mit.edu>
2019-11-09 22:56:02 -0500

    Fix deprecation warning.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Numbergossip::Application.config.secret_token = 'ca405d16ee75f7271fe8cd93fb69ede6d595de298463030a7a09c0ad26eece66a352d6409a41171c57b98e3e82ae825f04df1ff7b6bc055dda95d4536de4286f'
 Numbergossip::Application.config.secret_key_base = '6dcadf4bfd5a2b39bfb1bfadd4b9e2ff2b5ae6745ec54b70c34d9546e5149b9dd5293105365bacfb24a18c050be8bcb59c0305135a29369376d6ff918da81ce1'
