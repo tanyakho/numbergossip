24193c30c83a197417f19301128a3577d2db8711
axch <axch@users.noreply.github.com>
2025-07-04 19:18:19 +0000

    deploy: a29e071fc2478ffad7fbf9380f5f2561fd5694ca

---
+++ assets/application-27c19750b74dd27b228616162591e8118b4c16e49798d5c4c82354e809a0be9b.js
@@ -0,0 +1,27 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+var JRPEffect = {
+    toggle_blind: function() {
+        for (var i = 0; i < arguments.length; i++) {
+            var element = $(arguments[i]);
+            if(Element.visible(element)) {
+                Effect.BlindUp(element, {duration: 0.2 });
+            } else {
+                Effect.BlindDown(element, {duration: 0.2});
+            }
+        }
+    },
+
+    toggle_slide: function() {
+        for (var i = 0; i < arguments.length; i++) {
+            var element = $(arguments[i]);
+            if(Element.visible(element)) {
+                Effect.SlideUp(element, {duration: 0.2 });
+            } else {
+                Effect.SlideDown(element, {duration: 0.2});
+            }
+        }
+    }
+}
+;
